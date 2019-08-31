146d58ec9ad4f4b6f23409c9fda24f8c9efe2450
visual_community@outlook.com <visual_community@outlook.com>
2019-08-31 09:52:02 -0400

    web api ok

---
+++ Addressbook/apiAddressbook/Models/Datacontact.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace apiAddressbook.Models
+{
+    using System.Data.Entity;
+
+    public class Datacontact:DbContext
+    {
+        public Datacontact(): base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<apiAddressbook.Models.Book> Books { get; set; }
+    }
+}
